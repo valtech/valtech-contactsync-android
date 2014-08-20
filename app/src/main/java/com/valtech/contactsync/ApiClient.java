6c19c405a98307542beb8aac7640d7986d42282d
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-08-20 23:09:30 +0200

    Possible to add Valtech account to phone

---
+++ app/src/main/java/com/valtech/contactsync/ApiClient.java
@@ -0,0 +1,142 @@
+package com.valtech.contactsync;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiClient {
+  private static final Gson GSON = new Gson();
+
+  private static final String AUTHORIZE_URL = "https://stage-id.valtech.com/oauth2/authorize";
+  private static final String TOKEN_URL = "https://stage-id.valtech.com/oauth2/token";
+  private static final String USER_INFO_URL = "https://stage-id.valtech.com/api/users/me";
+  private static final String ALL_USER_INFOS_URL = "https://stage-id.valtech.com/api/users";
+  private static final String CLIENT_ID = "valtech.contactsync.android";
+  private static final String CLIENT_SECRET = "MGIxZmIwMDYtZTVlOS00ZDdiLTgxYjMtMjg2MzdhMTQwNGVh";
+  private static final String INITIAL_SCOPE = "email vidp:get_users vidp:offline";
+  private static final String FOLLOW_UP_SCOPE = "vidp:get_users";
+
+
+  public String getAuthorizeUrl() {
+    return AUTHORIZE_URL + "?response_type=code&client_id=" + CLIENT_ID + "&scope=" + INITIAL_SCOPE.replace(" ", "%20");
+  }
+
+  public TokenResponse getAccessTokenAndRefreshToken(String code) {
+    TokenResponse tokenResponse = tokenRequest(
+      new BasicNameValuePair("grant_type", "authorization_code"),
+      new BasicNameValuePair("code", code));
+    return tokenResponse;
+  }
+
+  public String getAccessToken(String refreshToken) {
+    TokenResponse tokenResponse = tokenRequest(
+      new BasicNameValuePair("grant_type", "refresh_token"),
+      new BasicNameValuePair("refresh_token", refreshToken),
+      new BasicNameValuePair("scope", FOLLOW_UP_SCOPE));
+    return tokenResponse.accessToken;
+  }
+
+  private TokenResponse tokenRequest(NameValuePair... fields) {
+    HttpClient httpClient = new DefaultHttpClient();
+    HttpPost httpPost = new HttpPost(TOKEN_URL);
+
+    try {
+      List<NameValuePair> nameValuePairs = new ArrayList<>();
+      nameValuePairs.add(new BasicNameValuePair("client_id", CLIENT_ID));
+      nameValuePairs.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
+      nameValuePairs.addAll(Arrays.asList(fields));
+
+      httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+      HttpResponse response = httpClient.execute(httpPost);
+
+      int statusCode = response.getStatusLine().getStatusCode();
+      if (statusCode >= 400 && statusCode < 500) {
+        String json = EntityUtils.toString(response.getEntity());
+        TokenErrorResponse tokenErrorResponse = GSON.fromJson(json, TokenErrorResponse.class);
+        throw new OAuthException(tokenErrorResponse);
+      } else if (statusCode >= 500) {
+        throw new RuntimeException("Internal server error occurred.");
+      }
+
+      String json = EntityUtils.toString(response.getEntity());
+      TokenResponse tokenResponse = GSON.fromJson(json, TokenResponse.class);
+
+      return tokenResponse;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public UserInfoResponse getUserInfoMeResource(String accessToken) {
+    return getProtectedResource(USER_INFO_URL, accessToken, UserInfoResponse.class);
+  }
+
+  private <T> T getProtectedResource(String url, String accessToken, Class<T> clazz) {
+    HttpClient httpClient = new DefaultHttpClient();
+    HttpGet httpGet = new HttpGet(url);
+
+    try {
+      httpGet.setHeader("Authorization", "Bearer " + accessToken);
+      HttpResponse response = httpClient.execute(httpGet);
+
+      int statusCode = response.getStatusLine().getStatusCode();
+      if (statusCode >= 400 && statusCode < 500) {
+        Header wwwAuthenticateHeader = response.getFirstHeader("WWW-Authenticate");
+        throw new OAuthException(wwwAuthenticateHeader);
+      } else if (statusCode >= 500) {
+        throw new RuntimeException("Internal server error occurred.");
+      }
+
+      String json = EntityUtils.toString(response.getEntity());
+      T typedResponse = GSON.fromJson(json, clazz);
+
+      return typedResponse;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static class TokenResponse {
+    @SerializedName("access_token")
+    public String accessToken;
+
+    @SerializedName("refresh_token")
+    public String refreshToken;
+  }
+
+  public static class TokenErrorResponse {
+    public String error;
+
+    @SerializedName("error_description")
+    public String error_description;
+  }
+
+  public static class UserInfoResponse {
+    public String email;
+  }
+
+  public static class OAuthException extends RuntimeException {
+    public OAuthException(TokenErrorResponse tokenErrorResponse) {
+      super("OAuth error occurred on token request ('" + tokenErrorResponse.error + "', '" + tokenErrorResponse.error_description + "').");
+    }
+
+    public OAuthException(Header wwwAuthenticateHeader) {
+      super("OAuth error occurred when accessing protected resource (" + wwwAuthenticateHeader.getValue() + ")");
+    }
+  }
+}
--- app/src/main/java/com/valtech/contactsync/SignInActivity.java
+++ app/src/main/java/com/valtech/contactsync/SignInActivity.java
@@ -1,28 +1,31 @@
 package com.valtech.contactsync;
 
+import android.accounts.Account;
 import android.accounts.AccountAuthenticatorActivity;
+import android.accounts.AccountManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import com.google.gson.Gson;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SignInActivity extends AccountAuthenticatorActivity {
-  private static final Gson GSON = new Gson();
+  public static final String ACCOUNT_TYPE = "com.valtech.contactsync.account";
+  private ApiClient apiClient = new ApiClient();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WebView webview = new WebView(this);
+    WebViewClient client = new IdpWebViewClient();
+    client.onPageFinished(webview, "vidp://callback");
+    webview.setWebViewClient(client);
+    setContentView(webview);
+
+    webview.loadUrl(apiClient.getAuthorizeUrl());
+  }
 
   private class IdpWebViewClient extends WebViewClient {
     @Override
@@ -40,57 +43,31 @@ public class SignInActivity extends AccountAuthenticatorActivity {
       Uri uri = Uri.parse(url);
       final String code = uri.getQueryParameter("code");
 
-      AsyncTask.SERIAL_EXECUTOR.execute(new Runnable() {
-        @Override
-        public void run() {
-          exchangeCodeForToken(code);
-        }
-      });
-
+      new SignInTask().execute(code);
     }
   }
 
-  private void exchangeCodeForToken(String code) {
-    HttpClient httpClient = new DefaultHttpClient();
-    HttpPost httpPost = new HttpPost("https://stage-id.valtech.com/oauth2/token");
+  private class SignInTask extends AsyncTask<String, Void, Bundle> {
+    @Override
+    protected Bundle doInBackground(String... params) {
+      ApiClient.TokenResponse tokenResponse = apiClient.getAccessTokenAndRefreshToken(params[0]);
+      ApiClient.UserInfoResponse userInfoResponse = apiClient.getUserInfoMeResource(tokenResponse.accessToken);
 
-    try {
-      List<NameValuePair> nameValuePairs = new ArrayList<>(2);
-      nameValuePairs.add(new BasicNameValuePair("client_id", "valtech.contactsync.android"));
-      nameValuePairs.add(new BasicNameValuePair("client_secret", "MGIxZmIwMDYtZTVlOS00ZDdiLTgxYjMtMjg2MzdhMTQwNGVh"));
-      nameValuePairs.add(new BasicNameValuePair("grant_type", "authorization_code"));
-      nameValuePairs.add(new BasicNameValuePair("code", code));
+      AccountManager accountManager = AccountManager.get(SignInActivity.this);
+      Account account = new Account(userInfoResponse.email, ACCOUNT_TYPE);
+      accountManager.addAccountExplicitly(account, tokenResponse.refreshToken, null);
 
-      httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-
-      HttpResponse response = httpClient.execute(httpPost);
-      String json = EntityUtils.toString(response.getEntity());
-      TokenResponse tokenResponse = GSON.fromJson(json, TokenResponse.class);
-
-      System.out.println("Access token: " + tokenResponse.access_token);
-      System.out.println("Refresh token: " + tokenResponse.refresh_token);
-
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+      Bundle result = new Bundle();
+      result.putString(AccountManager.KEY_ACCOUNT_NAME, userInfoResponse.email);
+      result.putString(AccountManager.KEY_ACCOUNT_TYPE, ACCOUNT_TYPE);
+      //result.putString(AccountManager.KEY_AUTHTOKEN, tokenResponse.refreshToken);
+      return result;
     }
-  }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    WebView webview = new WebView(this);
-    WebViewClient client = new IdpWebViewClient();
-    client.onPageFinished(webview, "vidp://callback");
-    webview.setWebViewClient(client);
-    setContentView(webview);
-
-    webview.loadUrl("https://stage-id.valtech.com/oauth2/authorize?response_type=code&client_id=valtech.contactsync.android&scope=vidp:get_users%20vidp:offline");
-  }
-
-  public static class TokenResponse {
-    public String access_token;
-    public String refresh_token;
+    @Override
+    protected void onPostExecute(Bundle result) {
+      SignInActivity.this.setAccountAuthenticatorResult(result);
+      SignInActivity.this.finish();
+    }
   }
 }
