756c0cae08153c84da94d2abfc2e6261531f0b89
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-08-26 23:10:16 +0200

    Valtech account settings now launches SettingsActivity

---
+++ app/src/main/java/com/valtech/contactsync/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.valtech.contactsync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.settings);
+  }
+}
