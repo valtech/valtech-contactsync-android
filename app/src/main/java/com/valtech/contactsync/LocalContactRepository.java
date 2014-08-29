186e0cad30d79d7e71f1b12f7a69a20348c556b1
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-08-29 15:36:30 +0200

    Handle delete/insert of contact's fields for existing contacts

--- app/src/main/java/com/valtech/contactsync/LocalContactRepository.java
+++ app/src/main/java/com/valtech/contactsync/LocalContactRepository.java
@@ -62,15 +62,7 @@ public class LocalContactRepository {
 
     ArrayList<ContentProviderOperation> ops = new ArrayList<>();
 
-    // Name
-    ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
-      .withSelection(
-        Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ?",
-        new String[]{String.valueOf(localContact.rawContactId), StructuredName.CONTENT_ITEM_TYPE})
-      .withValue(StructuredName.DISPLAY_NAME, remoteContact.name)
-      .build());
-
-    // Email
+    // Email, can never be missing, always update
     ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
       .withSelection(
         Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ?",
@@ -79,26 +71,80 @@ public class LocalContactRepository {
       .withValue(CommonDataKinds.Email.TYPE, CommonDataKinds.Email.TYPE_WORK)
       .build());
 
-    // Mobile phone
-    ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
-      .withSelection(
-        Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ? AND " + CommonDataKinds.Phone.TYPE + " = ?",
-        new String[] { String.valueOf(localContact.rawContactId), CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(CommonDataKinds.Phone.TYPE_WORK_MOBILE) })
-      .withValue(CommonDataKinds.Phone.NUMBER, remoteContact.phoneNumber)
-      .build());
-
-    // Fixed phone
-    ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
-      .withSelection(
-        Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ? AND " + CommonDataKinds.Phone.TYPE + " = ?",
-        new String[]{String.valueOf(localContact.rawContactId), CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(CommonDataKinds.Phone.TYPE_WORK)})
-      .withValue(CommonDataKinds.Phone.NUMBER, remoteContact.fixedPhoneNumber)
-      .build());
+    syncName(localContact, remoteContact, ops);
+    syncMobilePhoneNumber(localContact, remoteContact, ops);
+    syncFixedPhoneNumber(localContact, remoteContact, ops);
 
     try {
       resolver.applyBatch(ContactsContract.AUTHORITY, ops);
     } catch (Exception e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  private void syncName(LocalContactReader.LocalContact localContact, ApiClient.UserInfoResponse remoteContact, ArrayList<ContentProviderOperation> ops) {
+    if (nullOrEmpty(localContact.displayName) && !nullOrEmpty(remoteContact.name)) {
+      // missing on local contact, insert it
+      ops.add(buildDisplayNameInsert(remoteContact.name).withValue(Data.RAW_CONTACT_ID, localContact.rawContactId).build());
+    } else if (!nullOrEmpty(localContact.displayName) && nullOrEmpty(remoteContact.name)) {
+      // exists on local contact, but not on remote - delete it on local
+      ops.add(ContentProviderOperation.newDelete(DATA_CONTENT_URI)
+        .withSelection(
+          Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ?",
+          new String[]{String.valueOf(localContact.rawContactId), StructuredName.CONTENT_ITEM_TYPE})
+        .build());
+    } else if (!nullOrEmpty(localContact.displayName) && !nullOrEmpty(remoteContact.name)) {
+      // exists on both local and remote contact, update it
+      ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
+        .withSelection(
+          Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ?",
+          new String[]{String.valueOf(localContact.rawContactId), StructuredName.CONTENT_ITEM_TYPE})
+        .withValue(StructuredName.DISPLAY_NAME, remoteContact.name)
+        .build());
+    }
+  }
+
+  private void syncMobilePhoneNumber(LocalContactReader.LocalContact localContact, ApiClient.UserInfoResponse remoteContact, ArrayList<ContentProviderOperation> ops) {
+    if (nullOrEmpty(localContact.phoneNumber) && !nullOrEmpty(remoteContact.phoneNumber)) {
+      // missing on local contact, insert it
+      ops.add(buildPhoneNumberInsert(remoteContact.phoneNumber).withValue(Data.RAW_CONTACT_ID, localContact.rawContactId).build());
+    } else if (!nullOrEmpty(localContact.phoneNumber) && nullOrEmpty(remoteContact.phoneNumber)) {
+      // exists on local contact, but not on remote - delete it on local
+      ops.add(ContentProviderOperation.newDelete(DATA_CONTENT_URI)
+        .withSelection(
+          Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ? AND " + CommonDataKinds.Phone.TYPE + " = ?",
+          new String[]{String.valueOf(localContact.rawContactId), CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(CommonDataKinds.Phone.TYPE_WORK_MOBILE)})
+        .build());
+    } else if (!nullOrEmpty(localContact.phoneNumber) && !nullOrEmpty(remoteContact.phoneNumber)) {
+      // exists on both local and remote contact, update it
+      ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
+        .withSelection(
+          Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ? AND " + CommonDataKinds.Phone.TYPE + " = ?",
+          new String[]{String.valueOf(localContact.rawContactId), CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(CommonDataKinds.Phone.TYPE_WORK_MOBILE)})
+        .withValue(CommonDataKinds.Phone.NUMBER, remoteContact.phoneNumber)
+        .build());
+    }
+  }
+
+  private void syncFixedPhoneNumber(LocalContactReader.LocalContact localContact, ApiClient.UserInfoResponse remoteContact, ArrayList<ContentProviderOperation> ops) {
+    if (nullOrEmpty(localContact.fixedPhoneNumber) && !nullOrEmpty(remoteContact.fixedPhoneNumber)) {
+      // missing on local contact, insert it
+      ops.add(buildFixedPhoneNumberInsert(remoteContact.fixedPhoneNumber).withValue(Data.RAW_CONTACT_ID, localContact.rawContactId).build());
+    } else if (!nullOrEmpty(localContact.fixedPhoneNumber) && nullOrEmpty(remoteContact.fixedPhoneNumber)) {
+      // exists on local contact, but not on remote - delete it on local
+      ops.add(ContentProviderOperation.newDelete(DATA_CONTENT_URI)
+        .withSelection(
+          Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ? AND " + CommonDataKinds.Phone.TYPE + " = ?",
+          new String[]{String.valueOf(localContact.rawContactId), CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(CommonDataKinds.Phone.TYPE_WORK)})
+        .build());
+    } else if (!nullOrEmpty(localContact.fixedPhoneNumber) && !nullOrEmpty(remoteContact.fixedPhoneNumber)) {
+      // exists on both local and remote contact, update it
+      ops.add(ContentProviderOperation.newUpdate(DATA_CONTENT_URI)
+        .withSelection(
+          Data.RAW_CONTACT_ID + " = ? AND " + Data.MIMETYPE + " = ? AND " + CommonDataKinds.Phone.TYPE + " = ?",
+          new String[]{String.valueOf(localContact.rawContactId), CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(CommonDataKinds.Phone.TYPE_WORK)})
+        .withValue(CommonDataKinds.Phone.NUMBER, remoteContact.fixedPhoneNumber)
+        .build());
     }
   }
 
@@ -114,7 +160,7 @@ public class LocalContactRepository {
       .withValue(RawContacts.SOURCE_ID, remoteContact.email)
       .build());
 
-    // Email
+    // Email, always available from IDP
     ops.add(ContentProviderOperation.newInsert(DATA_CONTENT_URI)
       .withValueBackReference(Data.RAW_CONTACT_ID, backReferenceIndex)
       .withValue(Data.MIMETYPE, CommonDataKinds.Email.CONTENT_ITEM_TYPE)
@@ -124,30 +170,22 @@ public class LocalContactRepository {
 
     // Name
     if (!nullOrEmpty(remoteContact.name)) {
-      ops.add(ContentProviderOperation.newInsert(DATA_CONTENT_URI)
+      ops.add(buildDisplayNameInsert(remoteContact.name)
         .withValueBackReference(Data.RAW_CONTACT_ID, backReferenceIndex)
-        .withValue(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE)
-        .withValue(StructuredName.DISPLAY_NAME, remoteContact.name)
         .build());
     }
 
     // Mobile phone
     if (!nullOrEmpty(remoteContact.phoneNumber)) {
-      ops.add(ContentProviderOperation.newInsert(DATA_CONTENT_URI)
+      ops.add(buildPhoneNumberInsert(remoteContact.phoneNumber)
         .withValueBackReference(Data.RAW_CONTACT_ID, backReferenceIndex)
-        .withValue(Data.MIMETYPE, CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-        .withValue(CommonDataKinds.Phone.NUMBER, remoteContact.phoneNumber)
-        .withValue(CommonDataKinds.Phone.TYPE, CommonDataKinds.Phone.TYPE_WORK_MOBILE)
         .build());
     }
 
     // Fixed phone
     if (!nullOrEmpty(remoteContact.fixedPhoneNumber)) {
-      ops.add(ContentProviderOperation.newInsert(DATA_CONTENT_URI)
+      ops.add(buildFixedPhoneNumberInsert(remoteContact.fixedPhoneNumber)
         .withValueBackReference(Data.RAW_CONTACT_ID, backReferenceIndex)
-        .withValue(Data.MIMETYPE, CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-        .withValue(CommonDataKinds.Phone.NUMBER, remoteContact.fixedPhoneNumber)
-        .withValue(CommonDataKinds.Phone.TYPE, CommonDataKinds.Phone.TYPE_WORK)
         .build());
     }
 
@@ -163,6 +201,26 @@ public class LocalContactRepository {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private ContentProviderOperation.Builder buildDisplayNameInsert(String displayName) {
+    return ContentProviderOperation.newInsert(DATA_CONTENT_URI)
+      .withValue(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE)
+      .withValue(StructuredName.DISPLAY_NAME, displayName);
+  }
+
+  private ContentProviderOperation.Builder buildPhoneNumberInsert(String phoneNumber) {
+    return ContentProviderOperation.newInsert(DATA_CONTENT_URI)
+      .withValue(Data.MIMETYPE, CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+      .withValue(CommonDataKinds.Phone.NUMBER, phoneNumber)
+      .withValue(CommonDataKinds.Phone.TYPE, CommonDataKinds.Phone.TYPE_WORK_MOBILE);
+  }
+
+  private ContentProviderOperation.Builder buildFixedPhoneNumberInsert(String fixedPhoneNumber) {
+    return ContentProviderOperation.newInsert(DATA_CONTENT_URI)
+      .withValue(Data.MIMETYPE, CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+      .withValue(CommonDataKinds.Phone.NUMBER, fixedPhoneNumber)
+      .withValue(CommonDataKinds.Phone.TYPE, CommonDataKinds.Phone.TYPE_WORK);
   }
 
   private boolean nullOrEmpty(String s) {
@@ -203,9 +261,9 @@ public class LocalContactRepository {
     Cursor cursor = null;
     try {
       cursor = resolver.query(ContactsContract.Groups.CONTENT_URI,
-        new String[] { ContactsContract.Groups._ID },
+        new String[]{ContactsContract.Groups._ID},
         ContactsContract.Groups.ACCOUNT_TYPE + " = ? AND " + ContactsContract.Groups.TITLE + " = ?",
-        new String[] { account.type, groupTitle },
+        new String[]{account.type, groupTitle},
         null);
       if (cursor.moveToNext()) return cursor.getLong(0);
     } finally {
