9cd27dbf89cbf855f5491823643a54d100418151
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-03 23:56:08 +0200

    Add circleci configuration

---
+++ install-dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script installs the required SDK components for
+# building on CircleCI.
+
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  echo y | android update sdk -u -a -t android-20 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
+  touch $DEPS
+fi
