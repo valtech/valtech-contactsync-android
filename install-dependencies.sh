33f7903bc8890e0c6db41af59aeebd17ed4ac553
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-06 00:41:14 +0200

    Trigger gradlew earlier to it is stored in circle cache

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -12,5 +12,6 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t android-20 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t build-tools-20.0.0 &&
+  ./gradlew tasks && # trigger gradle wrapper download so it is stored in cache
   touch $DEPS
 fi
